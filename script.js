19bdc04f030dae6ac13c3a4825192f458e9d86b8
Helen Diana Formighieri <helendianaf@gmail.com>
2022-10-10 16:48:43 -0300

    new

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputUm = document.querySelector(".textoUm");
+const inputDois = document.querySelector(".textoDois");
+
+/* Codificando */
+function btnCodificar() {
+	const textoCodificado = codificar(inputUm.value);
+	inputDois.value = textoCodificado
+}
+
+
+function codificar(stringCodificada) {
+	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+	stringCodificada = stringCodificada.toLowerCase();
+
+	for(let i= 0; i < matrizCodigo.length; i++) {
+		if(stringCodificada.includes(matrizCodigo[i][0])) {
+			stringCodificada = stringCodificada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+		}
+
+	}
+	return stringCodificada;
+}
+
+
+/*Decodificando*/
+
+function btnDecodificar() {
+	const textoDecodificado = decodificar(inputUm.value)
+	inputDois.value = textoDecodificado
+}
+
+
+function decodificar(stringDecodificada) {
+	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+	stringDecodificada = stringDecodificada.toLowerCase();
+
+	for(let i= 0; i < matrizCodigo.length; i++) {
+		if(stringDecodificada.includes(matrizCodigo[i][1])) {
+			stringDecodificada = stringDecodificada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+		}
+
+	}
+	return stringDecodificada;
+}
+
+
+/* Botão para copiar */
+
+function copiar() {
+    const msg = document.querySelector(".textoDois");
+    msg.select();
+    navigator.clipboard.writeText(inputDois.value)
+    msg.value = "";
+}
